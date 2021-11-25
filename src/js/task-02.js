8012cca19f92a33b46067a783e01379acd59fecd
Shynal Nazar <Pro100nazar@i.ua>
2021-11-25 20:02:26 +0200

    Add task1 and task 2
    Готово два перші завдання

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const newitemEl = document.createElement("li");
+  newitemEl.classList.add("item");
+  newitemEl.textContent = ingredients[i];
+
+  list.append(newitemEl);
+}
