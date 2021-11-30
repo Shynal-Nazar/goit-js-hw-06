ce0d4aa2fa36a987ed5a4ea7070c9f3c4b281f66
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2021-11-30 20:19:58 +0200

    Add change 6task

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -5,6 +5,7 @@ inputEl.addEventListener("blur", () => {
 
     if (inputEl.value.length <= inputEl.getAttribute("data-length")) {
         inputEl.classList.add('invalid')
+        inputEl.classList.remove('valid')
     }
     else
         inputEl.classList.remove('invalid')
